a976f7f5e864ca75a95cb6a43d7845ba18ba6f4a
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2022-12-25 16:56:34 +0530

    Added solution - LeetHub

---
+++ Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.java
@@ -0,0 +1,150 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.maxPathSum(root));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Solution
+{
+    int res = Integer.MIN_VALUE;
+    
+    int maxPathSumUtil(Node root) {
+        if (root == null) 
+            return 0;
+        if (root.left == null && root.right == null) 
+            return root.data;
+
+        int ls = maxPathSumUtil(root.left);
+        int rs = maxPathSumUtil(root.right);
+
+        if (root.left != null && root.right != null) {
+            res = Math.max(res, (ls + rs + root.data));
+            return Math.max(ls, rs) + root.data;
+        }
+        if (root.left == null)
+            return rs + root.data;
+        else
+            return ls + root.data;
+    }
+
+    int maxPathSum(Node root) {
+        int h=maxPathSumUtil(root);
+        if(root.left==null || root.right==null)
+        {
+            res=Math.max(res,h);
+        }
+        return res;
+    }
+}
