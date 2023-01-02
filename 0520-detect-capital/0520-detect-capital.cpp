da3ffe5ba1c5c66415d6f01ab5d6186822e87056
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-01-02 08:17:06 +0530

    Time: 5 ms (24.37%), Space: 6.1 MB (70.23%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if (word.size() == 1) {
+            return true;
+        }
+        
+        if (word[0] < 91 && word[1] < 91) {
+            // Condition where the word should be all uppercase
+            for (int i = 2; i < word.size(); i++) {
+                if (word[i] > 96) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            // Condition where the word should be title case or lowercase
+            for (int i = 1; i < word.size(); i++) {
+                if (word[i] < 91) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+};
