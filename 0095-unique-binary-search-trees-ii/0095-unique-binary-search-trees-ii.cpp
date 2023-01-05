e5677fdb92bf0838f1e883003e339228da7db71b
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-01-05 17:03:24 +0530

    Time: 56 ms (5.16%), Space: 16 MB (65.52%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -10,16 +10,41 @@
  * };
  */
 class Solution {
-	public:
-		vector<TreeNode*> generateTrees(int n, int s = 1) {
-			vector<TreeNode*> ans;
-			if(n < s) return {nullptr};                              
-			 for(int i=s; i<=n; i++) {                	                 // Consider every number in range [s,n] as root  
-				for(auto left: generateTrees(i-1, s)) {                  // generate all possible trees in range [s,i)
-					for(auto right: generateTrees(n, i+1))               // generate all possible trees in range (i,e]
-						ans.push_back(new TreeNode(i, left, right));     // make new trees with i as the root 
-				}
+public:
+    vector<TreeNode*> buildTree(int start, int end) {
+	vector<TreeNode*> ans;
+        
+    // If start > end, then subtree will be empty so add NULL in the ans and return it.
+    if(start > end) {
+		ans.push_back(NULL);
+        return ans;
+    }
+
+    // Iterate through all values from start to end to construct left and right subtree recursively
+	for(int i = start; i <= end; ++i) {
+		vector<TreeNode*> leftSubTree = buildTree(start, i - 1);    // Construct left subtree
+        vector<TreeNode*> rightSubTree = buildTree(i + 1, end);     // Construct right subtree
+            
+		// loop through all left and right subtrees and connect them to ith root  
+		for(int j = 0; j < leftSubTree.size(); j++) {
+			for(int k = 0; k < rightSubTree.size(); k++) {
+				TreeNode* root = new TreeNode(i);   // Create root with value i
+				root->left = leftSubTree[j];   // Connect left subtree rooted at leftSubTree[j]
+                root->right = rightSubTree[k];   // Connect right subtree rooted at rightSubTree[k]
+				ans.push_back(root);    // Add this tree(rooted at i) to ans data-structure
 			}
-			return ans;
 		}
-	};
+    }
+        
+	return ans;
+}
+    
+vector<TreeNode*> generateTrees(int n) {
+	return buildTree(1, n);
+}
+
+
+
+
+
+};
