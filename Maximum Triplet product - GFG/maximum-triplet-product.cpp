63bb7d9c8e0f0666063a6818eac3dfff4f822649
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-03-10 08:22:08 +0530

    Added solution - LeetHub

--- Maximum Triplet product - GFG/maximum-triplet-product.cpp
+++ Maximum Triplet product - GFG/maximum-triplet-product.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+
 class Solution {
   public:
     long long maxTripletProduct(long long arr[], int n)
@@ -57,7 +58,6 @@ class Solution {
         return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 
