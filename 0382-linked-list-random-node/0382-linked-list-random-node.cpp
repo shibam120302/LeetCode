5023e286827522e3ebf750326cd4222d433ec85c
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-03-10 08:04:44 +0530

    Time: 25 ms (56.31%), Space: 16.6 MB (57.94%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* head;
+    
+public:
+    Solution(ListNode* head) {
+        this->head = head;
+        std::srand(std::time(0)); // seed the random number generator
+    }
+    
+    int getRandom() {
+        int count = 0;
+        int result = 0;
+        ListNode* curr = head;
+        
+        while (curr) {
+            count++;
+            // generate a random number between 1 and the count
+            // if the random number is 1, update the result with the current node's value
+            if (std::rand() % count == 0) {
+                result = curr->val;
+            }
+            curr = curr->next;
+        }
+        
+        return result;
+    }
+};
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
