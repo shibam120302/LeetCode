5e85f5b2582267330f7db64a472339c54e7f6fbf
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-03-17 08:08:44 +0530

    Added solution - LeetHub

---
+++ Number of Subarrays of 0's - GFG/number-of-subarrays-of-0s.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+long long int no_of_subarrays(int n, vector<int> &arr) {
+
+    long long int count = 0;
+    long long int answer = 0;
+    for (int i = 0; i < n; i++) {
+        
+        if(arr[i] == 0) {
+            count++;
+        }
+        else {
+            answer += (count*(count+1))/2;
+            count= 0;
+        }
+    }
+    answer += (count*(count+1))/2;
+    return answer;
+}
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cout << no_of_subarrays(n, arr) << endl;
+    }
+}
+
+// } Driver Code Ends
