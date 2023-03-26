2fa429a7822f6b0b608b8ef62220be42bd9d8e37
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-03-26 07:58:12 +0530

    Added solution - LeetHub

---
+++ Frogs and Jumps - GFG/frogs-and-jumps.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        
+        vector<int> leaveStatus (leaves + 1, 0);
+		for (int i = 0; i < N; i++) {
+			if (frogs[i] <= leaves && leaveStatus[frogs[i]] == 0) {
+				for (int j = frogs[i]; j <= leaves; j += frogs[i]) {
+					leaveStatus[j] = 1;
+				}
+			}
+		}
+		int leafCount = leaves;
+		for (int i : leaveStatus) {
+			if (i) {
+				leafCount--;
+			}
+		}
+		return leafCount;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, leaves;
+        cin >> N >> leaves;
+        int frogs[N];
+        for (int i = 0; i < N; i++) {
+            cin >> frogs[i];
+        }
+
+        Solution ob;
+        cout << ob.unvisitedLeaves(N, leaves, frogs) << endl;
+    }
+}
+// } Driver Code Ends
