932f81e9485569942c8a2de7bfa8f541b7d58042
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-03-30 09:03:50 +0530

    Added solution - LeetHub

---
+++ Minimum Integer - GFG/minimum-integer.py
@@ -0,0 +1,45 @@
+
+from typing import List
+
+
+class Solution:
+    def minimumInteger(self, N : int, A : List[int]) -> int:
+        # code here
+        sumo = sum(A)
+        ans = 10**9
+        for x in A:
+            if N*x >= sumo:
+                ans = min(ans, x)
+        return ans
+        
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        N = int(input())
+        
+        
+        A=IntArray().Input(N)
+        
+        obj = Solution()
+        res = obj.minimumInteger(N, A)
+        
+        print(res)
+        
+
+# } Driver Code Ends
