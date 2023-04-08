6af566fbd050e66eba0cca4644e4abba9f6f8480
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-04-08 09:09:45 +0530

    Added solution - LeetHub

--- Make the array beautiful - GFG/make-the-array-beautiful.cpp
+++ Make the array beautiful - GFG/make-the-array-beautiful.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-
 class Solution {
   public:
     bool check(int a, int b) {
@@ -24,6 +23,7 @@ class Solution {
         return st;
     }
 };
+
 
 //{ Driver Code Starts.
 
