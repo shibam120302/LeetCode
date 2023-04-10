bd210c71536c7f4fb8fc2ad8a387c2a07a422e46
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-04-10 07:56:20 +0530

    Added solution - LeetHub

---
+++ Maximum Intersecting Lines - GFG/maximum-intersecting-lines.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+    
+        map<int,int> lineMap;
+        for(vector<int> line : lines){
+            int start = line[0], end = line[1] + 1;
+        
+            lineMap[start]++;
+            lineMap[end]--;
+        }
+        int res = 0, cnt = 0;
+        for(auto entry : lineMap){
+            cnt += entry.second;
+            res = max(res, cnt);
+        }
+        return res;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N, vector<int>(2));
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < N; i++) {
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.maxIntersections(mat, N) << endl;
+    }
+}
+// } Driver Code Ends
