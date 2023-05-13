031ba1eac8e1ecab241f66ec1ffcda5b1ba1796c
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-05-13 09:00:08 +0530

    Added solution - LeetHub

---
+++ Bit Magic - GFG/bit-magic.py
@@ -0,0 +1,42 @@
+class Solution:
+    def bitMagic(self,n,arr):
+        c=0
+        for i in range(n):
+            if arr[i]!=arr[n-i-1]:
+                c+=1
+        c//=2
+        return (c+1)//2
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.bitMagic(n, arr)
+        
+        print(res)
+        
+
+# } Driver Code Ends
