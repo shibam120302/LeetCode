5aa418607c720d541030333b5bbb80429104a6d2
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-05-31 06:09:07 +0530

    Added solution - LeetHub

--- Frequency Game - GFG/frequency-game.cpp
+++ Frequency Game - GFG/frequency-game.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-
 class Solution{
 public:
     int LargButMinFreq(int arr[], int n) {
@@ -28,6 +27,7 @@ public:
         return mx;
     }
 };
+
 
 //{ Driver Code Starts.
 
