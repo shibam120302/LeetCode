7dbd60d52d3c732a7d2fd6884285013081155ad6
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-06-08 08:24:29 +0530

    Added solution - LeetHub

--- Find Kth permutation - GFG/find-kth-permutation.cpp
+++ Find Kth permutation - GFG/find-kth-permutation.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+
 class Solution
 {
 public:
@@ -43,7 +44,6 @@ public:
         return s;
     }
 };
-
 
 //{ Driver Code Starts.
 
