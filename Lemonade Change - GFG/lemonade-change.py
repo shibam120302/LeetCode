7f66a1ebec7835856b12775414a6fbb832269321
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-06-22 08:01:07 +0530

    Added solution - LeetHub

---
+++ Lemonade Change - GFG/lemonade-change.py
@@ -0,0 +1,36 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    def lemonadeChange(self, N, bills):
+        five = ten = 0
+        for i in bills:
+            if i == 5:
+                five += 1
+            elif i == 10:
+                five -= 1
+                ten += 1
+            elif ten > 0:
+                ten -= 1
+                five -= 1
+            else:
+                five -= 3
+            if five < 0:
+                return False
+        return True
+
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N = int(input())
+        bills = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.lemonadeChange(N, bills)
+        print(res)
+# } Driver Code Ends
