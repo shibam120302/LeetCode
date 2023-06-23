819d46df3ac011c3329175fc5451060b6ad6afb6
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-06-23 07:42:26 +0530

    Added solution - LeetHub

---
+++ Task Scheduler - GFG/task-scheduler.py
@@ -0,0 +1,30 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+from collections import Counter
+class Solution:
+    def leastInterval(self, N, K, tasks):
+        mp = Counter(tasks)
+        count = max(mp.values())
+        
+        ans = (count - 1) * (K + 1)
+        for key, val in mp.items():
+            if val == count:
+                ans += 1
+        return max(len(tasks), ans)
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        N, K = list(map(int, input().split()))
+        tasks = input().split()
+        ob = Solution()
+        res = ob.leastInterval(N, K, tasks)
+        print(res)
+# } Driver Code Ends
