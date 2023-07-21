6fe0d95b584c73f5e9844facc945e8f5b8ce2a58
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-07-21 08:18:00 +0530

    Time: 149 ms (88.83%), Space: 13.3 MB (25.03%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size(), maxlen = 1, ans = 0;
-        vector<int> cnt(n, 1), len(n, 1);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        vector<int> lengths(n, 1);  // Length of LIS ending at each index
+        vector<int> counts(n, 1);   // Number of LIS ending at each index
+
+        int max_length = 1;
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
-                    if (len[j]+1 > len[i]) {
-                        len[i] = len[j]+1;
-                        cnt[i] = cnt[j];
+                    if (lengths[i] == lengths[j] + 1) {
+                        counts[i] += counts[j]; // Another LIS ending at the same index
+                    } else if (lengths[i] < lengths[j] + 1) {
+                        lengths[i] = lengths[j] + 1; // Update length
+                        counts[i] = counts[j];      // Reset count for the new length
                     }
-                    else if (len[j]+1 == len[i]) 
-                        cnt[i] += cnt[j];
                 }
             }
-            maxlen = max(maxlen, len[i]);
+
+            max_length = max(max_length, lengths[i]);
         }
-        // find the longest increasing subsequence of the whole sequence
-       // sum valid counts
-        for (int i = 0; i < n; i++) 
-            if (len[i] == maxlen) ans += cnt[i];
-        return ans;
+
+        int result = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (lengths[i] == max_length) {
+                result += counts[i]; // Sum counts of LIS with the maximum length
+            }
+        }
+
+        return result;
     }
 };
+
