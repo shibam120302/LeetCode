9b2c5e90bcd14fba172dee30b8a300b1357b898f
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-07-24 08:00:20 +0530

    Added solution - LeetHub

---
+++ Right View of Binary Tree - GFG/right-view-of-binary-tree.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// } Driver Code Ends
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Should return  right view of tree
+class Solution
+{
+    public:
+    //Function to get the right view of the binary tree.
+    void rightViewUtil(vector<int> &vec,struct Node *root,int level,int *max_level)
+    {
+        //if root is null, we simply return.
+        if (root==NULL)  return;
+    
+         //if this is the last node of its level then it is in the right view.
+        if (*max_level < level)
+        {
+            vec.push_back(root->data);
+            *max_level = level;
+        }
+    
+        //calling function recursively for right and left 
+        //subtrees of current node.
+        rightViewUtil(vec, root->right, level+1, max_level);
+        rightViewUtil(vec, root->left, level+1, max_level);
+    }
+    
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(struct Node *root)
+    {
+        int max_level = 0;
+        vector<int> vec;
+        rightViewUtil(vec,root, 1, &max_level);
+        //returning the list.
+        return vec;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        Solution ob;
+        vector<int> vec = ob.rightView(root);
+        for(int x : vec){
+            cout<<x<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
