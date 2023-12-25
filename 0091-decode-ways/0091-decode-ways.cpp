1f72832c1001de855df11c8ceca0668c513c49c6
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2023-12-25 09:03:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (98.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n+1, 0);
-        dp[n] = 1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (s[i] != '0') dp[i] += dp[i+1];
-            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6'))
-                dp[i] += dp[i+2];
+        if (s.empty() || s[0] == '0') return 0;
+
+        int prev1 = 1, prev2 = 1;
+
+        for (int i = 1; i < s.length(); ++i) {
+            int oneDigit = stoi(s.substr(i, 1));
+            int twoDigits = stoi(s.substr(i - 1, 2));
+
+            int current = 0;
+            if (oneDigit != 0) current += prev1;
+            if (twoDigits >= 10 && twoDigits <= 26) current += prev2;
+
+            prev2 = prev1;
+            prev1 = current;
         }
-        return dp[0];
+
+        return prev1;
     }
 };
