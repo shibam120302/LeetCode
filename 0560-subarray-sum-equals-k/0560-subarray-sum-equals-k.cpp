b7016e6bb66daa0a742fac0680a98791f79b14a4
Shibam Nath <112779826+shibam120302@users.noreply.github.com>
2024-01-26 19:46:25 +0530

    Time: 57 ms (95.30%), Space: 45.1 MB (44.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,15 +1,32 @@
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int count = 0, sum = 0;
-        map<int, int> m;
-        m[0]++;
-        for (int i = 0; i < nums.size(); i++) {
+    int subarraySum(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        int sum = 0;
+
+        // HashMap to store cumulative sum frequencies
+        std::unordered_map<int, int> sumFreq;
+
+        // Initialize with 0 sum frequency to handle subarrays starting from the beginning
+        sumFreq[0] = 1;
+
+        for (int i = 0; i < n; ++i) {
             sum += nums[i];
-            count += m[sum - k];
-            m[sum]++;
+
+            // Check if there exists a cumulative sum (sum - k) in the hashmap
+            // If yes, add its frequency to the count
+            if (sumFreq.find(sum - k) != sumFreq.end()) {
+                count += sumFreq[sum - k];
+            }
+
+            // Update the cumulative sum frequency in the hashmap
+            sumFreq[sum]++;
         }
-        
+
         return count;
     }
 };
